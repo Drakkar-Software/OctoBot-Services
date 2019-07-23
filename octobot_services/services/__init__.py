fc9555745ba088e9907de8be9577bd4e3ea27a86
Herklos <herklos.dev@protonmail.com>
2019-07-23 18:33:50 +0200

    [Version] 0.0.0

---
+++ octobot_services/services/__init__.py
@@ -0,0 +1,21 @@
+#  Drakkar-Software OctoBot-Services
+#  Copyright (c) Drakkar-Software, All rights reserved.
+#
+#  This library is free software; you can redistribute it and/or
+#  modify it under the terms of the GNU Lesser General Public
+#  License as published by the Free Software Foundation; either
+#  version 3.0 of the License, or (at your option) any later version.
+#
+#  This library is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#  Lesser General Public License for more details.
+#
+#  You should have received a copy of the GNU Lesser General Public
+#  License along with this library.
+
+from octobot_services.services.service_creator import *
+from octobot_services.services.reddit_service import *
+from octobot_services.services.telegram_service import *
+from octobot_services.services.twitter_service import *
+from octobot_services.services.web_service import *
---
+++ setup.py
@@ -0,0 +1,66 @@
+#  Drakkar-Software OctoBot-Evaluators
+#  Copyright (c) Drakkar-Software, All rights reserved.
+#
+#  This library is free software; you can redistribute it and/or
+#  modify it under the terms of the GNU Lesser General Public
+#  License as published by the Free Software Foundation; either
+#  version 3.0 of the License, or (at your option) any later version.
+#
+#  This library is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#  Lesser General Public License for more details.
+#
+#  You should have received a copy of the GNU Lesser General Public
+#  License along with this library.
+# from distutils.extension import Extension
+from Cython.Build import cythonize
+from Cython.Distutils import build_ext
+from setuptools import find_packages
+from setuptools import setup, Extension
+import numpy as np
+
+from octobot_services import PROJECT_NAME, VERSION
+
+PACKAGES = find_packages(exclude=["tests"])
+
+packages_list: list = [""]
+
+ext_modules: list = [
+    Extension(package, [f"{package.replace('.', '/')}.py"])
+    for package in packages_list]
+
+# long description from README file
+with open('README.md', encoding='utf-8') as f:
+    DESCRIPTION = f.read()
+
+REQUIRED = open('requirements.txt').read()
+REQUIRES_PYTHON = '>=3.7'
+
+setup(
+    name=PROJECT_NAME,
+    version=VERSION,
+    url='https://github.com/Drakkar-Software/OctoBot-Services',
+    license='LGPL-3.0',
+    author='Drakkar-Software',
+    author_email='drakkar-software@protonmail.com',
+    description='OctoBot project services package',
+    packages=PACKAGES,
+    include_package_data=True,
+    long_description=DESCRIPTION,
+    install_requires=REQUIRED,
+    include_dirs=[np.get_include()],
+    cmdclass={'build_ext': build_ext},
+    tests_require=["pytest"],
+    test_suite="tests",
+    zip_safe=False,
+    data_files=[],
+    setup_requires=['Cython'],
+    python_requires=REQUIRES_PYTHON,
+    ext_modules=cythonize(ext_modules),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Cython',
+    ],
+)
