2acb1fb633f6527efb1ce2ba1844fb7159b478f5
Herklos <herklos.dev@protonmail.com>
2019-09-25 21:00:58 +0200

    Improved setup.py

--- setup.py
+++ setup.py
@@ -14,8 +14,15 @@
 #  You should have received a copy of the GNU Lesser General Public
 #  License along with this library.
 # from distutils.extension import Extension
-from Cython.Build import cythonize
-from Cython.Distutils import build_ext
+
+try:
+    from Cython.Distutils import build_ext
+except ImportError:
+    # create closure for deferred import
+    def build_ext(*args, **kwargs):
+        from Cython.Distutils import build_ext
+        return build_ext(*args, **kwargs)
+
 from setuptools import find_packages
 from setuptools import setup, Extension
 import numpy as np
@@ -24,9 +31,9 @@ from octobot_services import PROJECT_NAME, VERSION
 
 PACKAGES = find_packages(exclude=["tests"])
 
-packages_list: list = [""]
+packages_list = []
 
-ext_modules: list = [
+ext_modules = [
     Extension(package, [f"{package.replace('.', '/')}.py"])
     for package in packages_list]
 
@@ -34,7 +41,7 @@ ext_modules: list = [
 with open('README.md', encoding='utf-8') as f:
     DESCRIPTION = f.read()
 
-REQUIRED = open('requirements.txt').read()
+REQUIRED = open('requirements.txt').readlines()
 REQUIRES_PYTHON = '>=3.7'
 
 setup(
@@ -48,16 +55,16 @@ setup(
     packages=PACKAGES,
     include_package_data=True,
     long_description=DESCRIPTION,
-    install_requires=REQUIRED,
     include_dirs=[np.get_include()],
     cmdclass={'build_ext': build_ext},
     tests_require=["pytest"],
     test_suite="tests",
     zip_safe=False,
     data_files=[],
-    setup_requires=['Cython'],
+    setup_requires=REQUIRED,
+    install_requires=[],
+    ext_modules=ext_modules,
     python_requires=REQUIRES_PYTHON,
-    ext_modules=cythonize(ext_modules),
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Programming Language :: Python :: 3.7',
