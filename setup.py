504736c04be59f49a91976ff3457dcea5085f9cd
Herklos <herklos.dev@protonmail.com>
2019-12-21 18:54:42 +0100

    Upgraded OctoBot module structure

--- setup.py
+++ setup.py
@@ -15,25 +15,35 @@
 #  License along with this library.
 # from distutils.extension import Extension
 
+import os
+
+from setuptools import dist
+
+dist.Distribution().fetch_build_eggs(['Cython>=0.29.14'])
+
 try:
     from Cython.Distutils import build_ext
+    from Cython.Build import cythonize
 except ImportError:
     # create closure for deferred import
+    def cythonize(*args, **kwargs):
+        from Cython.Build import cythonize
+        return cythonize(*args, **kwargs)
+
     def build_ext(*args, **kwargs):
         from Cython.Distutils import build_ext
         return build_ext(*args, **kwargs)
 
 from setuptools import find_packages
 from setuptools import setup, Extension
-import numpy as np
 
 from octobot_services import PROJECT_NAME, VERSION
 
 PACKAGES = find_packages(exclude=["tests"])
 
-packages_list = []
+packages_list: list = []
 
-ext_modules = [
+ext_modules: list = [
     Extension(package, [f"{package.replace('.', '/')}.py"])
     for package in packages_list]
 
@@ -43,6 +53,7 @@ with open('README.md', encoding='utf-8') as f:
 
 REQUIRED = open('requirements.txt').readlines()
 REQUIRES_PYTHON = '>=3.7'
+CYTHON_DEBUG = False if not os.getenv('CYTHON_DEBUG') else os.getenv('CYTHON_DEBUG')
 
 setup(
     name=PROJECT_NAME,
