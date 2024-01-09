815f354e7d71b597996a0611d4ada7a0ba2357ea
Guillaume De Saint Martin <guillaumemdsm@gmail.com>
2024-01-09 19:03:17 +0100

    [Util] add is_openai_proxy

--- octobot_services/util/module_identification.py
+++ octobot_services/util/module_identification.py
@@ -1,4 +1,4 @@
-#  Drakkar-Software OctoBot-Interfaces
+#  Drakkar-Software OctoBot-Services
 #  Copyright (c) Drakkar-Software, All rights reserved.
 #
 #  This library is free software; you can redistribute it and/or
@@ -13,34 +13,8 @@
 #
 #  You should have received a copy of the GNU Lesser General Public
 #  License along with this library.
-import gc
-import octobot_services.util
-
-# ensure that imports that might be conflicting are importable
 
 
-def test_web_imports():
-    import flask
-    import flask_caching
-    import flask_compress
-    import flask_socketio
-    import gevent
-    import geventwebsocket
-    import flask_login
-    import wtforms
-    import flask_wtf
-
-
-def test_telegram_imports():
-    import telegram
-    import telethon
-
-
-def test_openai_imports():
-    import openai
-    proxies = [
-        type(obj)
-        for obj in gc.get_objects()
-        if octobot_services.util.is_openai_proxy(obj)
-    ]
-    assert len(proxies) > 5
+def is_openai_proxy(obj) -> bool:
+    str_type = str(type(obj))
+    return "openai." in str_type and "Proxy" in str_type
